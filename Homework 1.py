4680df2d4a60bcae37acf8ff5fafe23470651f2f
Legion978 <drkr2001@yandex.ru>
2023-10-08 13:35:06 +0300

    Add Homework_1 and Homework_2

---
+++ Homework 1.py
@@ -0,0 +1,27 @@
+
+#Задание 1
+My_age =21
+Now_year= 2023
+print("Год, в котором мне исполнится 77:",Now_year+56)
+
+import math
+food1 =2
+food2 =3
+food3 = 5
+
+#Задание 2
+size1 =math.sqrt(0.03)+math.log10(6)/3 - math.cos(food1)
+size2 =math.sqrt(0.03)+math.log10(6)/3 - math.cos(food2)
+size3 =math.sqrt(0.03)+math.log10(6)/3 - math.cos(food3)
+sum =size1+size2+size3
+print("Суммарный размер:",sum)
+print("Средний размер",sum/3)
+
+#Задание 3
+a=2
+b=4
+print("Длина стороны а:",a,"Длина сторны b:",b)
+P=a*2+b*2
+print("Периметр равен:",P)
+S=a*b
+print("Площадь равна:",S)
